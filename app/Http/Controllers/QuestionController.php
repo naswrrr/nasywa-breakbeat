c44916ba33b04395a71e032c5a151133c6dc5be5
naswrrr <nasywa24si@mahasiswa.pcr.ac.id>
2025-09-29 10:50:45 +0700

    Penerapan Laravel Validation

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -27,11 +26,20 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'nama'       => 'required|max:10',
+            'email'      => ['required', 'email'],
+            'pertanyaan' => 'required|max:300|min:8',
+        ],[
+            'nama.required'=>'Nama Tidak Boleh Kosong',
+            'email.email'=> 'Email Tidak Valid!'
+        ]);
+
         //dd($request->all());
 
-        $data['nama'] =$request->nama;
-        $data['email'] =$request->email;
-        $data['pertanyaan'] =$request->pertanyaan;
+        $data['nama']       = $request->nama;
+        $data['email']      = $request->email;
+        $data['pertanyaan'] = $request->pertanyaan;
 
         return view('home-question-respon', $data);
     }
