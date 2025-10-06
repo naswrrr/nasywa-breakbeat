a8de100bdf6b3bc9a9ebfd9873f958e6bdcb7e55
naswrrr <nasywa24si@mahasiswa.pcr.ac.id>
2025-10-06 09:54:59 +0700

    Penambahan Template Admin Volt

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MahasiswaController;
 
 Route::get('/', function () {
@@ -38,3 +39,5 @@ Route::get('/Pegawai', [PegawaiController::class, 'index']);
 
 Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
+
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
